d76ee42ca8a6bda434f961dfb825d882b84da9f1
biamaxine <bianca.maxine.dev@gmail.com>
2024-09-01 14:54:01 -0400

    feat: CONNECT MongoDB_Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@thebestcluster.bz8s21o.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority&appName=TheBestCluster`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
